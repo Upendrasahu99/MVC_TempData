deb5cf6f88e0265a09a74dfb5fde48b48254a0d1
Upendra Sahu <upendrasahu1199@gmail.com>
2023-09-25 12:56:26 +0530

    [Upendra] Add:Implemented_How_To_Use_Temp_Data

---
+++ TempData/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TempData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["myKey"] = "Data form Index";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            ViewBag.mykey = TempData["myKey"];
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            ViewBag.mykey = TempData["myKey"]; // we can access data for one request
+            return View();
+        }
+    }
+}
